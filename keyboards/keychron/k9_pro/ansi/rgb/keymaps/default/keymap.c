e40b0a355e73861c9fa93da01771a9f7afc8cbbd
Krystof Gartner <krystofgartner@proton.me>
2025-07-26 11:17:25 +0200

    Dip switch Windows layout

--- keyboards/keychron/k9_pro/ansi/rgb/keymaps/default/keymap.c
+++ keyboards/keychron/k9_pro/ansi/rgb/keymaps/default/keymap.c
@@ -16,15 +16,7 @@
 
 #include QMK_KEYBOARD_H
 
-enum layers {
-    BASE,
-    MEDIA,
-    VIM,
-    CHARS,
-    MICE,
-    NUMS,
-    CUSTOM_4,
-};
+enum layers { BASE_LINUX, BASE_WIN, MEDIA, VIM, CHARS, MICE, NUMS };
 
 #define ESC_CHAR LT(CHARS, KC_ESC)
 #define BCS_VIM LT(VIM, KC_BSPC)
@@ -32,6 +24,7 @@ enum layers {
 
 #define CTL_RET MT(MOD_LCTL, KC_ENT)
 #define WIN_SPC MT(MOD_LGUI, KC_SPC)
+#define CTL_ALT_SPC MT(MOD_RCTL | MOD_RALT, KC_SPC)
 
 #define VIM_B LCTL(KC_LEFT)
 #define VIM_E LCTL(KC_RGHT)
@@ -39,12 +32,21 @@ enum layers {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // TODO: tapdance for mouse button
-    [BASE] = LAYOUT_61_ansi(
+    [BASE_LINUX] = LAYOUT_61_ansi(
         KC_CAPS,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_VOLD,  KC_VOLU,  KC_BSPC,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     BCS_VIM,  KC_MPLY,  XXXXXXX,
         ESC_CHAR, KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     CTL_RET,  MO(MICE),           XXXXXXX,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  DOT_NUM,  KC_SLSH,            KC_RSFT,
         KC_LCTL,  KC_LWIN,  KC_LALT,                                WIN_SPC,                                KC_RALT,  MO(MEDIA), KC_PSCR, KC_RCTL),
+
+    // NOTE: layout for windows (set as default with OS layout DIP switch)
+    // only change so far is space hold behavior
+    [BASE_WIN] = LAYOUT_61_ansi(
+        KC_CAPS,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_VOLD,  KC_VOLU,  KC_BSPC,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     BCS_VIM,  KC_MPLY,  XXXXXXX,
+        ESC_CHAR, KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     CTL_RET,  MO(MICE),           XXXXXXX,
+        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  DOT_NUM,  KC_SLSH,            KC_RSFT,
+        KC_LCTL,  KC_LWIN,  KC_LALT,                                CTL_ALT_SPC,                            KC_RALT,  MO(MEDIA), KC_PSCR, KC_RCTL),
 
     // TODO: zoom / unzoom
     [VIM] = LAYOUT_61_ansi(
@@ -83,10 +85,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,            RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  NK_TOGG,  KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_DEL,             _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
 
-    [CUSTOM_4] = LAYOUT_61_ansi(
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
+    // [BLANK] = LAYOUT_61_ansi(
+    //     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    //     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    //     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+    //     _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+    //     _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
 };
