8978870537cac20c21b6daec2966d1cbac7dc720
Krystof Gartner <krystofgartner@proton.me>
2025-09-29 23:28:04 +0200

    fixed vim e + semicol

--- keyboards/keychron/k9_pro/ansi/rgb/keymaps/default/keymap.c
+++ keyboards/keychron/k9_pro/ansi/rgb/keymaps/default/keymap.c
@@ -21,6 +21,7 @@ enum layers { BASE_LINUX, BASE_WIN, MEDIA, VIM, CHARS, MICE, NUMS };
 #define ESC_CHAR LT(CHARS, KC_ESC)
 #define BCS_VIM LT(VIM, KC_BSPC)
 #define DOT_NUM LT(NUMS, KC_DOT)
+#define SEM_MICE LT(MICE, KC_SCLN)
 
 #define CTL_RET MT(MOD_LCTL, KC_ENT)
 #define WIN_SPC MT(MOD_LGUI, KC_SPC)
@@ -35,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE_LINUX] = LAYOUT_61_ansi(
         KC_CAPS,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_VOLD,  KC_VOLU,  KC_BSPC,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     BCS_VIM,  KC_MPLY,  XXXXXXX,
-        ESC_CHAR, KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     CTL_RET,  MO(MICE),           XXXXXXX,
+        ESC_CHAR, KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     CTL_RET,  SEM_MICE,           XXXXXXX,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  DOT_NUM,  KC_SLSH,            KC_RSFT,
         KC_LCTL,  KC_LWIN,  KC_LALT,                                WIN_SPC,                                KC_RALT,  MO(MEDIA), KC_PSCR, KC_RCTL),
 
@@ -44,14 +45,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE_WIN] = LAYOUT_61_ansi(
         KC_CAPS,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_VOLD,  KC_VOLU,  KC_BSPC,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     BCS_VIM,  KC_MPLY,  XXXXXXX,
-        ESC_CHAR, KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     CTL_RET,  MO(MICE),           XXXXXXX,
+        ESC_CHAR, KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     CTL_RET,  SEM_MICE,           XXXXXXX,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  DOT_NUM,  KC_SLSH,            KC_RSFT,
         KC_LCTL,  KC_LWIN,  KC_LALT,                                CTL_ALT_SPC,                            KC_RALT,  MO(MEDIA), KC_PSCR, KC_RCTL),
 
     // TODO: zoom / unzoom
     [VIM] = LAYOUT_61_ansi(
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PGUP,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  VIM_E,    _______,  _______,  _______,  KC_PGUP,  _______,  _______,  _______,  _______,  _______,  _______,
         _______,  _______,  _______,  KC_PGDN,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  _______,            _______,
         _______,            KC_HOME,  KC_END,   _______,  _______,  VIM_B,    _______,  _______,  _______,  _______,  _______,            _______,
         _______,  _______,  _______,                                KC_ENT,                                 _______,  _______,  _______,  _______),
